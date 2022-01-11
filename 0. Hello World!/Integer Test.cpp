8c464113144a460810b8b7e44bf6511386976b96
dsteves28 <dsteves28@yahoo.com>
2022-01-10 19:54:30 -0600

    Initial_Commit

---
+++ 0. Hello World!/Integer Test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a;
+  cout << "Enter a number" << endl;
+    
+  // User can input an integer
+  cin >> a; 
+  cout << "User entered number " << a << endl;
+}
