d514a3d2abc61357f2692071eec7272e8256ce9c
dsteves28 <dsteves28@yahoo.com>
2022-01-19 14:54:39 -0600

    Initial_Commit

---
+++ 01. Lab_01/Lab_01.cpp
@@ -0,0 +1,16 @@
+// Donald Stephens
+// CSCI 201: Lab assignment 1
+#include <iostream>  
+using namespace std;
+
+const double PI = 3.14159265; //Set PI as constant 
+
+int main () {
+    double radius, volume; //Set variables radius and volume as double.
+    cout << "Enter the radius of a sphere: ";  //Asking for radius of sphere input
+    cin >> radius;  //Input radius of sphere
+    volume = 4.0/3.0 * PI * radius * radius * radius; //Equation to find the volume
+    cout << "The spheres volume is: " << volume << endl; //Outputting volune
+    return 0; // Fin
+}
+
