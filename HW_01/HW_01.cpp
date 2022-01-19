bfaa21911fcfc614f67a3bb5abb6de0d43c0fc9d
dsteves28 <dsteves28@yahoo.com>
2022-01-19 13:38:56 -0600

    Initial_Commit

---
+++ HW_01/HW_01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+include namespace std;
+
+int main()
+{
+    int smalldi, largedi;
+    double smallprice, largeprice;
+    
+    cout<<"Welcome to the Pizza Consumers!" <<endl;
+    
+    cout<<"Enter the diameter of a small pizza (in inches): ";
+    cin >> smalldi;
+    cout<<"Enter the price of a small pizza ";
+    cin >> smallprice;
+    
+    cout<<"Enter the diameter of a large pizza (in inches): ";
+    cin >> largedi;
+    cout<<"Enter the price of a large pizza: ";
+    cin >> largeprice;
+    
+    cout<<"Small Pizza: " <<endl;
+    cout<<"Diameter = "<<smalldi;
+    
+
+    return 0;
+}
